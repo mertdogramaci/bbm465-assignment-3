d277451f8196c32514ffd0d3d3968c167cd3560e
mertdogramaci <mertdogramaci@gmail.com>
2022-12-02 18:32:29 +0300

    came to the license manager part

--- src/Client.java
+++ src/Client.java
@@ -1,20 +1,199 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileStore;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 
 public class Client {
-    private static final String LICENSE_FILE_PATH = "license.txt";
-    private String publicKey;
+    private final String LICENSE_FILE_PATH = "license.txt";
+    private final String KEYS_PATH = "keys/";
+    private PublicKey publicKey;
+    private String username;
+    private String serialNumber;
+    private String macAddress;
+    private String diskSerialNumber;
+    private String motherboardSerialNumber;
+    private String license;
 
     public Client() {
+        setUsername(System.getProperty("user.name"));
 
+        licenseFileChecker();
+
+        setLicense(username + "$" +
+                serialNumber + "$" +
+                macAddress + "$" +
+                diskSerialNumber + "$" +
+                motherboardSerialNumber);
+
+        System.out.println(getLicense());
+
+        byte[] encryptedLicenseBytes = getEncryptedLicense();
+
+        LicenseManager licenseManager = new LicenseManager(encryptedLicenseBytes);
     }
 
-    private static void licenseFileChecker() {
+    private void licenseFileChecker() {
         File licenseFile = new File(LICENSE_FILE_PATH);
-        System.out.println(licenseFile.exists());
+        if (licenseFile.exists()) {
+
+        } else {
+            setMacAddress();
+            setDiskSerialNumber();
+            setMotherboardSerialNumber();
+
+            setPublicKey();
+
+        }
+    }
+
+    private byte[] getEncryptedLicense() {
+        try {
+            Cipher encryptCipher = Cipher.getInstance("RSA");
+            encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] secretMessageBytes = license.getBytes(StandardCharsets.UTF_8);
+            byte[] encryptedLicenseBytes = encryptCipher.doFinal(secretMessageBytes);
+
+            return encryptedLicenseBytes;
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | IllegalBlockSizeException |
+                 BadPaddingException exception) {
+            exception.printStackTrace();
+        }
+
+        return new byte[0];
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey() {
+        try {
+            byte[] keyBytes = Files.readAllBytes(Paths.get(KEYS_PATH + "public.key"));
+            X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+
+            this.publicKey = kf.generatePublic(spec);
+        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public void setMacAddress() {
+        try {
+            InetAddress localHost = InetAddress.getLocalHost();
+            NetworkInterface networkInterface = NetworkInterface.getByInetAddress(localHost);
+            byte[] hardwareAddress = networkInterface.getHardwareAddress();
+
+            String[] hexadecimal = new String[hardwareAddress.length];
+
+            for (int i = 0; i < hardwareAddress.length; i++) {
+                hexadecimal[i] = String.format("%02X", hardwareAddress[i]);
+            }
+
+            this.macAddress = String.join(":", hexadecimal);
+        } catch (UnknownHostException | SocketException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public String getDiskSerialNumber() {
+        return diskSerialNumber;
+    }
+
+    public void setDiskSerialNumber() {
+        String diskSerialNumber = "";
+
+        FileStore store = FileSystems.getDefault().getFileStores().iterator().next();   // TODO: What if two disks exist?
+
+        try {
+            diskSerialNumber = store.getAttribute("volume:vsn").toString();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        this.diskSerialNumber = diskSerialNumber;
+    }
+
+    public String getMotherboardSerialNumber() {
+        return motherboardSerialNumber;
+    }
+
+    public void setMotherboardSerialNumber() {
+        String command = "wmic baseboard get serialnumber";
+
+        String serialNumber = "";
+
+        try {
+            Process SerialNumberProcess = Runtime.getRuntime().exec(command);
+
+            InputStreamReader ISR = new InputStreamReader(SerialNumberProcess.getInputStream());
+            BufferedReader br = new BufferedReader(ISR);
+
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                if (line.length() < 1 || line.startsWith("SerialNumber")) {
+                    continue;
+                }
+                serialNumber = line;
+                break;
+            }
+
+            br.close();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        this.motherboardSerialNumber = serialNumber;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
     }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        licenseFileChecker();
+        new Client();
     }
 }
--- src/LicenseManager.java
+++ src/LicenseManager.java
@@ -1,5 +1,26 @@
-public class LicenseManager {
-    public LicenseManager() {
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
+public class LicenseManager {
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public LicenseManager(byte[] encryptedLicenseBytes) {
+        try {
+            Cipher decryptCipher = Cipher.getInstance("RSA");
+            decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] decryptedMessageBytes = decryptCipher.doFinal(encryptedLicenseBytes);
+            String decryptedMessage = new String(decryptedMessageBytes, StandardCharsets.UTF_8);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | BadPaddingException |
+                 IllegalBlockSizeException exception) {
+            exception.printStackTrace();
+        }
     }
 }
